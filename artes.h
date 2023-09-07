9a845ad7ecdaa83fe41f2c64ce41850142212d2c
Kamagori <marcos.biscotto@ufv.br>
2023-09-07 15:11:03 -0300

    declaração das funções da tela

---
+++ artes.h
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#ifndef TP0_PAA_Artes_h
+#define TP0_PAA_Artes_h
+
+typedef struct tela{
+
+    int linha, coluna;
+    char **matriz;
+
+}Tela;
+
+void CriaTelaEmBranco(Tela *tela);
+void ImprimeTela(Tela *tela);
+void CriaAtesrisco(Tela *tela, int quantos);
+void CriaMais(Tela *tela, int quantos);
+void CriaXis(Tela *tela, int quantos);
+void PintaFiguraAleatoria(Tela *tela, int quantos);
+void CriaTriforce(Tela *tela, int quantos);
+int EscolhaFigura(Tela *tela, int escolha);
+
+#endif //TP0_PAA_Artes_h
